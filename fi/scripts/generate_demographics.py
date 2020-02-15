321af2468d5acdc88c0af50c25c06158deed0c25
xxshambm <ext.michael.shamberger@tieto.com>
2020-02-15 14:11:54 +0200

    fix code to generate good demographics file

--- fi/scripts/generate_demographics.py
+++ fi/scripts/generate_demographics.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # load the paavo csv into a dataframe
 df = pd.read_csv('../data/paavo_municipality_demographics.csv', dtype=object) 
-df = df.fillna("")
+df = df.fillna(0)
 df = df.rename(columns={'Inhabitants, total, 2018 (HE)': 'poptotal'})  # make shorter column name
 
 # load the posti key file so we can match municipality with region
@@ -13,13 +13,15 @@ keydf = keydf[['Name of the region','NAME']]
 keydf = keydf.drop_duplicates(subset=['Name of the region','NAME'])
 
 # create new dataframe columns with the data in synthea demographics format
-df.loc[:,'COUNTY'] = ""
+df["ID"] = df.index.values
+df["COUNTY"] = df.index.values
 df["NAME"] = df["Area"]
 df = pd.merge(df.astype(str),keydf.astype(str), on='NAME', how='left')
 df["poptotal"] = df["poptotal"].astype(int)
 df["STNAME"] = df["Name of the region"]
 df["POPESTIMATE2015"] = df["poptotal"]
 df["CTYNAME"] = df["Area"]
+df["TOT_POP"] = df["poptotal"]
 df["TOT_MALE"] = df["Males, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df["TOT_FEMALE"] = df["Females, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df.loc[:,'WHITE'] = 1.0
@@ -28,10 +30,13 @@ df.loc[:,'BLACK'] = 0.0
 df.loc[:,'ASIAN'] = 0.0
 df.loc[:,'NATIVE'] = 0.0
 df.loc[:,'OTHER'] = 0.0
-df["1"] = (df["0-2 years, 2018 (HE)"]+df["3-6 years, 2018 (HE)"]).astype(int).divide(df["poptotal"])  # should be 0-4 but data does not fit
-df["2"] = df["7-12 years, 2018 (HE)"].astype(int).divide(df["poptotal"]) # should be 5-9 but data does not fit
-df["3"] = df["13-15 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
-df["4"] = (df["16-17 years, 2018 (HE)"]+df["18-19 years, 2018 (HE)"]).astype(int).divide(df["poptotal"])
+# our data does not match synthea age brackets for ages 0-19 so we will take average of these 
+# for years 0-19.  After age 20 we get good match
+df["pre20"] = df["0-2 years, 2018 (HE)"].astype(int) + df["3-6 years, 2018 (HE)"].astype(int) + df["7-12 years, 2018 (HE)"].astype(int) + df["7-12 years, 2018 (HE)"].astype(int) + df["16-17 years, 2018 (HE)"].astype(int) + df["18-19 years, 2018 (HE)"].astype(int)
+df["1"] = df["pre20"].divide(df["poptotal"]).divide(4)
+df["2"] = df["pre20"].divide(df["poptotal"]).divide(4)
+df["3"] = df["pre20"].divide(df["poptotal"]).divide(4)
+df["4"] = df["pre20"].divide(df["poptotal"]).divide(4)
 df["5"] = df["20-24 years, 2018 (HE)"].astype(int).divide(df["poptotal"])  # good matching data from here
 df["6"] = df["25-29 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df["7"] = df["30-34 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
@@ -46,15 +51,16 @@ df["15"] = df["70-74 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df["16"] = df["75-79 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df["17"] = df["80-84 years, 2018 (HE)"].astype(int).divide(df["poptotal"])
 df["18"] = df["85 years or over, 2018 (HE)"].astype(int).divide(df["poptotal"])
-df.loc[:,'00..10'] = .19
-df.loc[:,'10..15'] = .1145
-df.loc[:,'15..25'] = .3
-df.loc[:,'25..35'] = .2
-df.loc[:,'35..50'] = .1465
-df.loc[:,'50..75'] = .03
-df.loc[:,'100..150'] = .012
-df.loc[:,'150..200'] = .005
-df.loc[:,'200..999'] = .001
+df.loc[:,'00..10'] = .19  # these numbers are estimates
+df.loc[:,'10..15'] = .1145 # these numbers are estimates
+df.loc[:,'15..25'] = .3 # these numbers are estimates
+df.loc[:,'25..35'] = .2 # these numbers are estimates
+df.loc[:,'35..50'] = .1 # these numbers are estimates
+df.loc[:,'50..75'] = .055 # these numbers are estimates
+df.loc[:,'75..100'] = .0265 # these numbers are estimates
+df.loc[:,'100..150'] = .008 # these numbers are estimates
+df.loc[:,'150..200'] = .005 # these numbers are estimates
+df.loc[:,'200..999'] = .001 # these numbers are estimates
 df['HS_DEGREE'] = df['Matriculation examination, 2018 (KO)'].astype(int).divide(df["poptotal"])
 df['SOME_COLLEGE'] = df['Vocational diploma, 2018 (KO)'].astype(int).divide(df["poptotal"])
 df['BS_DEGREE'] = (df['Academic degree - Lower level university degree, 2018 (KO)'].astype(int) + df['Academic degree - Higher level university degree, 2018 (KO)'].astype(int)).divide(df["poptotal"])
@@ -63,5 +69,5 @@ df['LESS_THAN_HS'] = df['LESS_THAN_HS'] - df['SOME_COLLEGE'] - df['BS_DEGREE'] -
 
 
 # write the new dataframe to the demographics file
-header = ["COUNTY","NAME","STNAME","POPESTIMATE2015","CTYNAME","TOT_POP","TOT_MALE","TOT_FEMALE","WHITE","HISPANIC","BLACK","ASIAN","NATIVE","OTHER","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","00..10","10..15","15..25","25..35","35..50","50..75","100..150","150..200","200..999","LESS_THAN_HS","HS_DEGREE","SOME_COLLEGE","BS_DEGREE"]
-df.to_csv('../src/main/resources/geography/demographics.csv', columns = header)
+header = ["ID","COUNTY","NAME","STNAME","POPESTIMATE2015","CTYNAME","TOT_POP","TOT_MALE","TOT_FEMALE","WHITE","HISPANIC","BLACK","ASIAN","NATIVE","OTHER","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","00..10","10..15","15..25","25..35","35..50","50..75","75..100","100..150","150..200","200..999","LESS_THAN_HS","HS_DEGREE","SOME_COLLEGE","BS_DEGREE"]
+df.to_csv('../src/main/resources/geography/demographics.csv', columns = header, index=False)
